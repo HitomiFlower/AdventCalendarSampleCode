8aa992e63c239cc537cb58c759a0247b39c08f5d
Wang Kang <anheikuaiyun001@gmail.com>
2018-11-17 12:32:02 +0900

    Modify behavior

--- Assets/Scripts/Const.cs
+++ Assets/Scripts/Const.cs
@@ -3,5 +3,5 @@ public static class Const
 {
 	public const float TopBound = 300f;
 	public const float BottomBound = -300f;
-	public const int EnemyIncrement = 200;
+	public const int EnemyIncrement = 1000;
 }
--- Assets/Scripts/ECS/BootStapper.cs
+++ Assets/Scripts/ECS/BootStapper.cs
@@ -5,6 +5,7 @@ using ECS;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Rendering;
 using Unity.Transforms;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -13,13 +14,13 @@ public class BootStapper : MonoBehaviour
 {
     private EntityManager _entityManager;
 
-    [SerializeField]
-    private GameObject _shipPrefab;
+    [SerializeField] private GameObject _shipPrefab;
 
-    public quaternion angle;
+    [SerializeField] private Mesh _mesh;
+    [SerializeField] private Material _material;
 
-    [HideInInspector]
-    public int count = 0;
+    [HideInInspector] public int count = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -40,18 +41,31 @@ public class BootStapper : MonoBehaviour
     {
         count += Const.EnemyIncrement;
         Debug.Log("Count: " + count);
-        NativeArray<Entity> entities = new NativeArray<Entity>(amount, Allocator.Temp);
-        _entityManager.Instantiate(_shipPrefab, entities);
+        //NativeArray<Entity> entities = new NativeArray<Entity>(amount, Allocator.Temp);
+        //_entityManager.Instantiate(_shipPrefab, entities);
+
+        var archeType = _entityManager.CreateArchetype(typeof(Position), typeof(Rotation), typeof(Speed));
 
         for (int i = 0; i < amount; i++)
         {
+            var entity = _entityManager.CreateEntity(archeType);
+
             float xVal = Random.Range(-500f, 500f);
             float zVal = Random.Range(-300f, 300f);
-            _entityManager.SetComponentData(entities[i], new Position{ Value = new float3(xVal, 0f, zVal)});
-            _entityManager.SetComponentData(entities[i], new Rotation {Value = angle});
-            _entityManager.SetComponentData(entities[i], new Speed{Value = Random.Range(10f, 30f)});
+            _entityManager.SetComponentData(entity, new Position {Value = new float3(xVal, 0f, zVal)});
+            _entityManager.SetComponentData(entity, new Rotation {Value = new quaternion(0, 1, 0, 0)});
+            _entityManager.SetComponentData(entity, new Speed {Value = Random.Range(10f, 30f)});
+            _entityManager.AddSharedComponentData(entity, CreateMesh());
         }
-        
-        entities.Dispose();
+        //entities.Dispose();
     }
+
+    private MeshInstanceRenderer CreateMesh()
+    {
+        var meshRenderer = new MeshInstanceRenderer();
+        meshRenderer.mesh = _mesh;
+        meshRenderer.material = _material;
+        return meshRenderer;
+    }
+
 }
